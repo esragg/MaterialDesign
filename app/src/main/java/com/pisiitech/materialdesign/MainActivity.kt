2626f2610862db9c829ee05f86cae79085bce6be
esragg <esragarcia@hotmail.com>
2023-07-23 17:24:34 +0200

    TopAppBar Kullanimi, Status Bar ve TopAppBar renk degisimi, Baslik, Alt Baslik ekleme

---
+++ app/src/main/java/com/pisiitech/materialdesign/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.pisiitech.materialdesign
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.pisiitech.materialdesign.ui.theme.MaterialDesignTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialDesignTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SayfaTopAppBar()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaterialDesignTheme {
+        SayfaTopAppBar()
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SayfaTopAppBar() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Column {
+                        Text(text = "Baslik")
+                        Text(text = "Alt Baslik", fontSize = 12.sp)
+                        }
+                    },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = colorResource(id = R.color.anaRenk),
+                    titleContentColor = Color.White
+                )
+            )
+        },
+        content = {
+
+        }
+    )
+}
+
