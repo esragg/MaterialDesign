40d4cf4268d44a10570c0031df50f33b709a2d41
esragg <esragarcia@hotmail.com>
2023-07-25 13:21:32 +0200

    Card Kullanimi

--- app/src/main/java/com/pisiitech/materialdesign/MainActivity.kt
+++ app/src/main/java/com/pisiitech/materialdesign/MainActivity.kt
@@ -5,9 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,11 +34,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pisiitech.materialdesign.ui.theme.MaterialDesignTheme
 
@@ -42,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SayfaTopAppBarArama()
+                    SayfaCard()
                 }
             }
         }
@@ -53,7 +65,32 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     MaterialDesignTheme {
-        SayfaTopAppBarArama()
+        SayfaCard()
+    }
+}
+
+@Composable
+fun SayfaCard() {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Card(modifier = Modifier
+            .padding(all = 10.dp)
+            .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Blue),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            border = BorderStroke(2.dp, Color.Magenta)
+            ) {
+            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.clickable {
+                Log.e("Card","Tiklandi")
+            })  {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(all = 10.dp).fillMaxWidth()) {
+                    Image(painter = painterResource(id = R.drawable.gunes_resim), contentDescription = "")
+                    Text(text = "Gunes", color = Color.White, fontSize = 36.sp)
+                    }
+                }
+            }
     }
 }
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
